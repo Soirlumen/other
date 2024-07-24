adc366608da843fa84d9e719f100ca42a6915354
Mirka <115395048+Soirlumen@users.noreply.github.com>
2024-07-24 13:24:50 +0200

    Update data_processing.py
    structure changes

--- data_processing.py
+++ data_processing.py
@@ -6,6 +6,13 @@ from scipy.optimize import curve_fit
 import numpy as np
 from scipy.integrate import quad
 
+#todo: otisk času vytvoření souboru výsledky + záměna čárek za tečky kvůli excelu
+#todo: rozdělení skriptu do vhodných podsouborů
+#todo: lehké uživatelské rozhraní (vložení adresáře s daty, navolení co daty udělat)
+#todo: zachycení chyb a následné automatické mazání všeho bordelu
+# (všechny adresáře vytvořené za běhu skriptu, jejich rozpoznání) při neúspěšném dokončení skriptu
+
+
 def func(x, a, b, c, d):
     return a * x**3 + b * x**2 + c * x + d
 
@@ -27,6 +34,7 @@ def nelin_regrese(df):
     x_max = xko[-1]  # Poslední hodnota x
     #integralka = numericky(x_max, a, b, c, d)
     return analyticky(x_max, a, b, c, d)
+
 def num_int(df):
     df = df.dropna(how='all')
     xko = np.array(pd.to_numeric(df[0], errors='coerce'))
@@ -42,7 +50,7 @@ def novy_adresar(jmeno, adresar):
     p = Path(adresar) / jmeno
     p.mkdir(exist_ok=True)
     if any(p.iterdir()):
-        print("Adresář 'temp/' existuje a obsahuje soubory. Skript je ukončen.")
+        print(f"Adresář {jmeno} existuje a obsahuje soubory. Skript je ukončen.")
         exit()
     return p
 
@@ -54,31 +62,36 @@ def kopirovani(puvodni, adresar):
                 shutil.copy(listek.path,tam)
 
 #se kterými sloupci souboru chci pracovat
-def vyjmout_sloupecky(df):
-    return df[[2, 3]]
+def vyjmout_sloupecky(df, o):
+    return df[o]
 
 #pokud mám soubor, ktery má desetinne carky, je treba zaměnit za desetinne tecky
-def nahradit_carky_za_tecky(df):
+def nahrazeni(df,o,u):
+    #o je, co nahrazujeme, u je za co nahrazujeme
     #return df.applymap(lambda x: x.replace(',', '.') if isinstance(x, str) else x)
-    return df.map(lambda x: x.replace(',', '.') if isinstance(x, str) else x)
+    return df.map(lambda x: x.replace(o, u) if isinstance(x, str) else x)
 
-def zpracovat_soubor(soubor_path, cesta_k_vysledku):
+#upraví soubory tak, abych na ně mohla pustit skripty :>
+def upravit_soubor(soubor_path):
     df = pd.read_csv(soubor_path, sep='\t', header=None, encoding='utf-8')
-    df = vyjmout_sloupecky(df)
-    df = nahradit_carky_za_tecky(df)
+    df = vyjmout_sloupecky(df,[2,3])
+    df = nahrazeni(df,',','.')
     df.to_csv(soubor_path, sep='\t', index=False, header=False, encoding='utf-8')
-    #nelin_regrese(soubor_path, cesta_k_vysledku)
+    return df
+
+def zpracovat_vysledky(soubor_path, cesta_k_vysledku, *argv):
     df = pd.read_csv(soubor_path, sep='\t', header=None, encoding='utf-8')
-    vysl=num_int(df)
-    vysl2=nelin_regrese(df)
+    vysl = np.array([funkce(df) for funkce in argv])
+    rozdily = np.abs(np.diff(vysl))
     with open(cesta_k_vysledku, 'a', encoding='utf-8') as f:
-        f.write(f"{Path(soubor_path).name}\t{vysl}\t{vysl2}\t{abs(vysl-vysl2)}\n")
+        f.write(f"{Path(soubor_path).name}\t" + "\t".join(map(str, vysl)) + "\t" + "\t".join(map(str, rozdily)) + "\n")
 
 def prochazet_adresare(cesta_k_vysledku,adresar):
     with os.scandir(adresar) as stromek:
         for listek in stromek:
             if listek.is_file() and listek.name.endswith('.txt'):
-                zpracovat_soubor(listek.path, cesta_k_vysledku)
+                upravit_soubor(listek.path)
+                zpracovat_vysledky(listek.path, cesta_k_vysledku, num_int, nelin_regrese)
 
 #smaze adresar a vsechny polozky v nem
 def uklizeci_ceta(adresar):
@@ -93,8 +106,11 @@ def uklizeci_ceta(adresar):
     print(f"adresar {adresar} byl smazan :> gg")
 
 ###################################
+
+print(os.getcwd())
 adresar = 'D:/dulezite_mimo_skolu/vupp_prougrami/17_7_0_nahrani'
 os.chdir(adresar)
+print(os.getcwd())
 
 vysledkovy_adresar=novy_adresar("vysledkovy_adresar",adresar)
 vysledky="vysledky1.txt"
