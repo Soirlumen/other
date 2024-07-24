6431fdf27223e0ca6e7fc3769c1f345c7268a834
Mirka <115395048+Soirlumen@users.noreply.github.com>
2024-07-24 10:29:55 +0200

    Update data_processing.py
    changes in structure of code and added some methods

--- data_processing.py
+++ data_processing.py
@@ -15,8 +15,7 @@ def numericky(x_max, a, b, c, d):
 def analyticky(x_max,a,b,c,d):
     return d*x_max+c*(x_max**2)/2+b*(x_max**3)/3+a*(x_max**4)/4
 
-def nelin_regrese(filename,cesta_k_vysledku):
-    df = pd.read_csv(filename, sep='\t', header=None)
+def nelin_regrese(df):
     df = df.dropna(how='all')
     #bere první dva sloupečky, předpokládá x souřadnice a f(x)
     xko = np.array(df[0])
@@ -26,26 +25,33 @@ def nelin_regrese(filename,cesta_k_vysledku):
     a, b, c, d = popt
 
     x_max = xko[-1]  # Poslední hodnota x
-    integral_value = numericky(x_max, a, b, c, d)
-    kontrola = analyticky(x_max, a, b, c, d)
+    #integralka = numericky(x_max, a, b, c, d)
+    return analyticky(x_max, a, b, c, d)
+def num_int(df):
+    df = df.dropna(how='all')
+    xko = np.array(pd.to_numeric(df[0], errors='coerce'))
+    yko = np.array(pd.to_numeric(df[1], errors='coerce'))
+    vysl=0
+    for i in range(1,len(xko)):
+        o=(xko[i]-xko[i-1])*yko[i-1]
+        vysl+=o
+    return vysl
 
-    with open(cesta_k_vysledku, 'a', encoding='utf-8') as f:
-        f.write(f"{Path(filename).name}\t{integral_value}\t{kontrola}\t{abs(integral_value - kontrola)}\n")
-
-def temp_adresar(adresar):
-    #zkontroluje a vytvori docasny adresar
-    p = Path(adresar) / "temp"
+def novy_adresar(jmeno, adresar):
+    #zkontroluje a vytvori adresar
+    p = Path(adresar) / jmeno
     p.mkdir(exist_ok=True)
     if any(p.iterdir()):
         print("Adresář 'temp/' existuje a obsahuje soubory. Skript je ukončen.")
         exit()
-    #zkopiruje do nej vsechny soubory z aktualniho adresare
-    with os.scandir(adresar) as stromek:
-        for listik in stromek:
-            if listik.is_file():
-                dest_path = os.path.join(p, listik.name)
-                shutil.copy(listik.path, dest_path)
     return p
+
+def kopirovani(puvodni, adresar):
+    with os.scandir(puvodni) as stromek:
+        for listek in stromek:
+            if listek.is_file():
+                tam=os.path.join(adresar,listek.name)
+                shutil.copy(listek.path,tam)
 
 #se kterými sloupci souboru chci pracovat
 def vyjmout_sloupecky(df):
@@ -61,8 +67,12 @@ def zpracovat_soubor(soubor_path, cesta_k_vysledku):
     df = vyjmout_sloupecky(df)
     df = nahradit_carky_za_tecky(df)
     df.to_csv(soubor_path, sep='\t', index=False, header=False, encoding='utf-8')
-    #co actually chci dělat s daty o.o
-    nelin_regrese(soubor_path, cesta_k_vysledku)
+    #nelin_regrese(soubor_path, cesta_k_vysledku)
+    df = pd.read_csv(soubor_path, sep='\t', header=None, encoding='utf-8')
+    vysl=num_int(df)
+    vysl2=nelin_regrese(df)
+    with open(cesta_k_vysledku, 'a', encoding='utf-8') as f:
+        f.write(f"{Path(soubor_path).name}\t{vysl}\t{vysl2}\t{abs(vysl-vysl2)}\n")
 
 def prochazet_adresare(cesta_k_vysledku,adresar):
     with os.scandir(adresar) as stromek:
@@ -74,10 +84,10 @@ def prochazet_adresare(cesta_k_vysledku,adresar):
 def uklizeci_ceta(adresar):
     os.chdir(adresar)
     with os.scandir(os.getcwd()) as stromek:
-        for listik in stromek:
-            if listik.is_file():
-                os.remove(listik.name)
-                print(f"soubor {listik.name} byl smazan")
+        for listecek in stromek:
+            if listecek.is_file():
+                os.remove(listecek.name)
+                print(f"soubor {listecek.name} byl smazan")
     os.chdir('..')
     os.rmdir(adresar)
     print(f"adresar {adresar} byl smazan :> gg")
@@ -85,13 +95,12 @@ def uklizeci_ceta(adresar):
 ###################################
 adresar = 'D:/dulezite_mimo_skolu/vupp_prougrami/17_7_0_nahrani'
 os.chdir(adresar)
-#vytvorime text soubor s vysledky
-vysledky="vysledky.txt"
-cesta_k_vysledku = os.path.join(adresar, vysledky)
 
-#vytvoreni docasneho adresare, kde budeme upravovat data dle potreb, pokud neexistuje
-p = temp_adresar(adresar)
-
-prochazet_adresare(cesta_k_vysledku,p)
+vysledkovy_adresar=novy_adresar("vysledkovy_adresar",adresar)
+vysledky="vysledky1.txt"
+cesta_k_vysledku = os.path.join(vysledkovy_adresar, vysledky)
+p = novy_adresar("temp",adresar)
+kopirovani(adresar,p)
+prochazet_adresare(cesta_k_vysledku, p)
 
 uklizeci_ceta(p)
